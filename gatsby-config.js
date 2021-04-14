bdc173ed9b9770203e561541f667ce9360ada0c4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-14 15:18:10 +0000

    Add site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Mid Wales Arts | Gallery, Sculpture Trail, Cafe & Accommodation`,
+    description: `Not-for-profit organisation working with artists and the local community to develop access to the arts across Mid Wales.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mwa-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
