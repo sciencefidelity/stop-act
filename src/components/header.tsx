8cb1ba07f944d7f8ad8651e7ad1bf5f1a2a49f26
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-15 01:00:03 +0000

    Add initial styles and fonts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,9 @@ import { StaticImage } from "gatsby-plugin-image"
 const Header = ({ siteTitle }) => (
   <header>
     <div>
-
+      <h1 className={`brand`}>Mid</h1>
+      <h1 className={`brand`}>Wales</h1>
+      <h1>Arts</h1>
     </div>
   </header>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div>
     
-      <h1>Hello world!</h1>
-      
+    </div>
   </Layout>
 )
 
