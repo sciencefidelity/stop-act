f42755bd4ea8b76503b1f9b26ed1a984221597e4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-19 02:54:45 +0000

    Add Sanity source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
         icon: `src/images/mwa-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `e5f80r5a`,
+        dataset: `production`,
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: 'default',
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
   ],
 }
