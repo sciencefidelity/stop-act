1298c95bdd09399af2ed5a3bfd7dc1be79ddf498
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-04-20 00:27:36 +0100

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,7 @@ import { StaticImage } from "gatsby-plugin-image"
 const Header = ({ siteTitle }) => (
   <header>
     <div>
-      <h1 className={`brand`}>Mid</h1>
-      <h1 className={`brand`}>Wales</h1>
+      <h1 className={`brand`}>Matt Cook Webart</h1>
       <h1>Arts</h1>
     </div>
   </header>
